17183d83b97055e767e090977d781d94409231fa
nebelgrau <misiek@gmx.it>
2022-06-13 15:35:31 +0200

    moved LSM9DS1 methods into lib.rs, improved enums' value() function

--- src/interrupts/pins_config.rs
+++ src/interrupts/pins_config.rs
@@ -1,8 +1,5 @@
 /// Functions related to INT1_AG and INT2_AG interrupt pins configuration
 ///
-/// TO DO:
-/// -
-///
 use super::*;
 
 /// Accelerometer/gyroscope interrupt pins common settings
@@ -26,7 +23,7 @@ impl Default for PinConfig {
 
 impl PinConfig {
     /// Returns values to be written to CTRL_REG8 register
-    fn ctrl_reg8(&self) -> u8 {
+    pub(crate) fn ctrl_reg8(&self) -> u8 {
         let mut data: u8 = 0;
 
         data |= self.active_level.value() << 5;
@@ -75,7 +72,7 @@ impl Default for IntConfigAG1 {
 
 impl IntConfigAG1 {
     /// Returns values to be written to INT1_CTRL register
-    fn int1_ctrl(&self) -> u8 {
+    pub(crate) fn int1_ctrl(&self) -> u8 {
         let mut data: u8 = 0;
 
         data |= self.enable_gyro_int.value() << 7;
@@ -124,7 +121,7 @@ impl Default for IntConfigAG2 {
 
 impl IntConfigAG2 {
     /// Returns values to be written to INT2_CTRL register
-    fn int2_ctrl(&self) -> u8 {
+    pub(crate) fn int2_ctrl(&self) -> u8 {
         let mut data: u8 = 0;
 
         data |= self.enable_fss5.value() << 5;
@@ -135,144 +132,6 @@ impl IntConfigAG2 {
         data |= self.enable_accel_dataready.value();
 
         data
-    }
-}
-
-impl<T> LSM9DS1<T>
-where
-    T: Interface,
-{
-    /// Enable interrupts for accelerometer/gyroscope and configure the INT1_A/G interrupt pin
-    pub fn configure_interrupts_ag1(&mut self, config: IntConfigAG1) -> Result<(), T::Error> {
-        self.interface.write(
-            Sensor::Accelerometer,
-            register::AG::INT1_CTRL.addr(),
-            config.int1_ctrl(),
-        )?;
-        Ok(())
-    }
-
-    /// Enable interrupts for accelerometer/gyroscope and configure the INT1_A/G interrupt pin
-    pub fn configure_interrupts_ag2(&mut self, config: IntConfigAG2) -> Result<(), T::Error> {
-        let reg_data = self.read_register(Sensor::Accelerometer, register::AG::INT2_CTRL.addr())?;
-
-        let mut data: u8 = reg_data & !0b1100_0000;
-
-        data |= config.int2_ctrl();
-
-        self.interface
-            .write(Sensor::Accelerometer, register::AG::INT2_CTRL.addr(), data)?;
-        Ok(())
-    }
-
-    /// Interrupt pins electrical configuration
-    pub fn configure_interrupts_pins(&mut self, config: PinConfig) -> Result<(), T::Error> {
-        let reg_data = self.read_register(Sensor::Accelerometer, register::AG::CTRL_REG8.addr())?;
-
-        let mut data: u8 = reg_data & !0b0011_0000;
-
-        data |= config.ctrl_reg8();
-
-        self.interface
-            .write(Sensor::Accelerometer, register::AG::CTRL_REG8.addr(), data)?;
-        Ok(())
-    }
-
-    /// Get the current A/G1 pin configuration
-    pub fn get_ag1_config(&mut self) -> Result<IntConfigAG1, T::Error> {
-        let reg_value: u8 =
-            self.read_register(Sensor::Accelerometer, register::AG::INT1_CTRL.addr())?;
-
-        let config = IntConfigAG1 {
-            enable_gyro_int: match (reg_value & 0b1000_0000) >> 7 {
-                1 => Flag::Enabled,
-                _ => Flag::Disabled,
-            },
-            enable_accel_int: match (reg_value & 0b0100_0000) >> 6 {
-                1 => Flag::Enabled,
-                _ => Flag::Disabled,
-            },
-            enable_fss5: match (reg_value & 0b0010_0000) >> 5 {
-                1 => Flag::Enabled,
-                _ => Flag::Disabled,
-            },
-            enable_overrun: match (reg_value & 0b0001_0000) >> 4 {
-                1 => Flag::Enabled,
-                _ => Flag::Disabled,
-            },
-            enable_fth: match (reg_value & 0b0000_1000) >> 3 {
-                1 => Flag::Enabled,
-                _ => Flag::Disabled,
-            },
-            enable_boot_status: match reg_value & 0b0000_0100 >> 2 {
-                1 => Flag::Enabled,
-                _ => Flag::Disabled,
-            },
-            enable_gyro_dataready: match reg_value & 0b0000_0010 >> 1 {
-                1 => Flag::Enabled,
-                _ => Flag::Disabled,
-            },
-            enable_accel_dataready: match reg_value & 0b0000_0001 {
-                1 => Flag::Enabled,
-                _ => Flag::Disabled,
-            },
-        };
-
-        Ok(config)
-    }
-
-    /// Get the current A/G2 pin configuration
-    pub fn get_ag2_config(&mut self) -> Result<IntConfigAG2, T::Error> {
-        let reg_value: u8 =
-            self.read_register(Sensor::Accelerometer, register::AG::INT2_CTRL.addr())?;
-
-        let config = IntConfigAG2 {
-            enable_fss5: match (reg_value & 0b0010_0000) >> 5 {
-                1 => Flag::Enabled,
-                _ => Flag::Disabled,
-            },
-            enable_overrun: match (reg_value & 0b0001_0000) >> 4 {
-                1 => Flag::Enabled,
-                _ => Flag::Disabled,
-            },
-            enable_fth: match (reg_value & 0b0000_1000) >> 3 {
-                1 => Flag::Enabled,
-                _ => Flag::Disabled,
-            },
-            enable_temp_dataready: match reg_value & 0b0000_0100 >> 2 {
-                1 => Flag::Enabled,
-                _ => Flag::Disabled,
-            },
-            enable_gyro_dataready: match reg_value & 0b0000_0010 >> 1 {
-                1 => Flag::Enabled,
-                _ => Flag::Disabled,
-            },
-            enable_accel_dataready: match reg_value & 0b0000_0001 {
-                1 => Flag::Enabled,
-                _ => Flag::Disabled,
-            },
-        };
-
-        Ok(config)
-    }
-
-    /// Get the current common pins configuration
-    pub fn get_pins_config(&mut self) -> Result<PinConfig, T::Error> {
-        let reg_value: u8 =
-            self.read_register(Sensor::Accelerometer, register::AG::CTRL_REG8.addr())?;
-
-        let config = PinConfig {
-            active_level: match (reg_value & 0b0100_0000) >> 5 {
-                1 => IntActive::Low,
-                _ => IntActive::High,
-            },
-            pin_mode: match (reg_value & 0b0010_0000) >> 4 {
-                1 => IntPin::OpenDrain,
-                _ => IntPin::PushPull,
-            },
-        };
-
-        Ok(config)
     }
 }
 
